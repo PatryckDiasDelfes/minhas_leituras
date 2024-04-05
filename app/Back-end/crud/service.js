580a784868dae1fc2685a83dd3e786e79c2adaa5
Patryck Dias Delfes <delfesne@gmail.com>
2024-04-05 13:23:19 -0300

    a

--- app/Back-end/crud/service.js
+++ app/Back-end/crud/service.js
@@ -1,6 +1,6 @@
 'use server'
 
-import sql from "./db";
+import sql from "@/app/lib/db";
 
 export async function insert(itens) {
   return await sql`INSERT INTO obras (titulo, autor, resenha) VALUES (${itens.titulo}, ${itens.autor}, ${itens.resenha})` 
--- app/Front-end/components/crud.tsx
+++ app/Front-end/components/crud.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react"; 
+import { useState } from "react";
+import { insert } from "@/app/Back-end/crud/service" 
 
-export default function Crud() {
+export default function Crud(props:{itens:any[]}) {
+
+    let [itens, setItens] = useState(props.itens)
  return(
     <>
         <section className="flex items-center justify-center bg-[#262626] h-screen w-screen">
             <div className="w-[55%] h-[70%] p-6 px-8 rounded bg-[#F2F2F2]">
-                adsfasdf
+                <div className="px-6">
+                    <div className="flex gap-5">
+                        <h1>Obras</h1>
+                        <span>Autor</span>
+                    </div>
+                </div>
                 <div className="flex items-center px-6 justify-between h-[15%] rounded-lg bg-[#F25A38] bg-opacity-35">
                     <div className="flex gap-5">
                         <h1>Nome livro</h1>
